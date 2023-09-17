275ac6528ee35ff201c6bf9185c27c80e67aaf9f
Ruslan Volovenko <vokoloven@gmail.com>
2023-09-17 23:41:40 +0300

    fix: removed func

--- src/components/form/formHandlers.js
+++ src/components/form/formHandlers.js
@@ -1,19 +1,3 @@
-export const handleSubmitNewMessage = (
-  socket,
-  message,
-  setMessage,
-  addToChatConversations,
-  dispatch,
-  e,
-) => {
-  e.preventDefault();
-  socket.emit('askCoach', [{ role: 'user', content: message }]);
-  dispatch(
-    addToChatConversations({ id: Date.now(), role: 'user', content: message }),
-  );
-  setMessage('');
-};
-
 export const handleChange = (setMessage, e) => {
   setMessage(e.target.value);
 };
